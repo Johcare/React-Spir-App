49c121a6b081621e133c2b7f12aa98649426b883
Graham Lawing <graham.lawing@gmail.com>
2017-10-03 17:50:47 -0400

    Working on navbar

--- client/components/Main.js
+++ client/components/Main.js
@@ -4,7 +4,19 @@ import {Link} from 'react-router';
 const Main = React.createClass({
   render() {
     return(
-    <div>
+      <div>
+      <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <ul className="nav navbar-nav">
+            <li><Link to={"/home"}>Home</Link></li>
+            <li><Link to={"/user"}>User</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
         <h1>
           <Link to="/">Spir()</Link>
         </h1>
