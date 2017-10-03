3ec63a7294aec9504359bebf3e4d61c175e20513
Johcare <johcare2@gmail.com>
2017-10-03 13:59:18 -0400

    Saer

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,19 +1,37 @@
 import React from 'react';
-
 import { render } from 'react-dom';
-
 // Import css
 import css from './styles/style.styl';
+
+
+
+
 
 // Import Components
 import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+
+
+
 // import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
+
+// IMORTING RAVEN.JS
+import Raven from 'raven-js';
+import  {sentry_url} from './data/config';
+
+Raven.config('https://297beb4a54aa4cf5ac7486c5d2381b01@sentry.io/225498').install();
+console.log(window.onload);
+
+Raven.context(function() {
+  initMyApp();
+});
+
+
 
 const router = (
   <Provider store={store}>
