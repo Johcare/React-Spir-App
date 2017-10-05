fa131f83c3f4f2db19f4f5390766d8111154aa57
Johcare <johcare2@gmail.com>
2017-10-05 14:09:39 -0400

    JSON from giphy,but not probl.with async,await

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Photo from './Photo';
+import axios from 'axios';
+import connect from 'react-redux'
 const PhotoGrid = React.createClass({
-  render() {
-    return( 
-    <div className="photo-grid">
-        {this.props.posts.map((post, i) => <Photo {... this.props} key={i} i={i} post={post}/>)}
-      </div>
-    )
+
+dataGetter(cb) {
+    axios
+      .get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => response.data)
+      .then(stuuf => {
+        let dataPPics =[]
+            stuuf.data.forEach(function(element) {
+
+              dataPPics.push(element.images.original_still.url);
+            });
+
+       cb(dataPPics);    
+      });
+
+},
+  
+render() {
+  let data = this.dataGetter(function(pic){
+      console.log(pic);
+     
+  });
+    return <div className="photo-grid">
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
+      </div>;
+
+
+
   }
 });
+
+//export default connect(MapStateToProps,{Actions})(PhotoGrid)
 
 export default PhotoGrid;
