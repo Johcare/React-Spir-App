afc2c16c9afa859d41586299b23e5262becba360
Graham Lawing <graham.lawing@gmail.com>
2017-10-05 21:19:57 -0400

    added Navbar

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Link} from 'react-router'; 
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import css from '../styles/Navbar.styl';
 
 
 const Main = React.createClass({
@@ -7,24 +8,32 @@ const Main = React.createClass({
     return(
       <div>
         {/* Nav Bar */}
-      <nav className="navbar navbar-default">
-      <div className="container">
-        <div className="navbar-header">
-          <ul className="nav navbar-nav">
-            <li><Link to={"/"} activeStyle={{color: "red"}}>Home</Link></li>
-            <li><Link to={"/user"}>User</Link></li>
-            <li><Link to={"/submit"}>Submit</Link></li>
-            <li><Link to={"/ranking"}>Ranking</Link></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+              <nav className="navbar navbar-default">
+                <div className="navbar-header">
+                  <ul>
+                  <li className="dropdown">
+                    <a href="javascript:void(0)" className="dropbtn">SpIr</a>
+                    <div className="dropdown-content">
+                        <a href="#">Submit</a>
+                        <a href="#">Rankings</a>
+                    </div>
+                    </li>
+                  </ul>
+                </div>
+                <ul className="nav navbar-nav">
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                <li><button className="btn btn-danger log">Log out </button></li>
+                  <li><button className="btn btn-info log">Log In</button></li>                  
+                </ul>
+              </nav>
 
-
+<div>
         <h1>
           <Link to="/">Spir()</Link>
         </h1>
         {React.cloneElement(this.props.children,this.props)}
+      </div>
       </div>
     )
   }
